c613050898df16581da3f9d5cd871042d8fd2405
Nachiket V Sarode <nachiketvsarode@gmail.com>
2024-02-18 19:40:03 -0500

    branch-2 first commit

--- src/lectures/demo/arrays/arrays.go
+++ src/lectures/demo/arrays/arrays.go
@@ -2,11 +2,7 @@ package main
 
 func main() {
 	type UserID int
-
 	type Direction byte
-	
-	
 	type Speed float64
-	
 	type Velocity Speed
 }
--- src/lectures/demo/if-else/if-else.go
+++ src/lectures/demo/if-else/if-else.go
@@ -1,10 +1,9 @@
 package main
 
-import "fmt"
-
 func average(a, b, c int) float32 {
 	// Convert the sum of the scores into a float32
-	return float32(a+b+c) / 3
+
+	return float32(a+b+c) / 3 ##############
 }
 
 func main() {
